d97c8b7654cd3c2e08e69aee650bc6111a320b5f
xzsaw <sawxuezheng01@gmail.com>
2016-09-01 14:56:38 -0400

    added structs

--- Lab02/Robot.c
+++ Lab02/Robot.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Struct deifinitions
+struct Workspace{
+   char pos[4][4];
+};
+
+struct Robot{
+   int pos_x;
+   int pos_y;
+};
+
+struct GoldBar{
+   int pos_x;
+   int pos_y;
+}
+
+struct Bomb{
+   int pos_x;
+   int pos_y;
+}
+
+
 // API?
 void API();
 void API()
@@ -16,13 +38,15 @@ void createWorld()
 }
 // Rand Positions
 
-// Run4Gold
+
+
+	// Run4Gold
 	// Check for available gold
 	// Check next Square
 	// Check for bomb
 	// jump next Square
 	// Check Gold
-		// Get Gold
+	// Get Gold
 
 int main()
 {
