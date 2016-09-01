16e67d8bd22c65870ae38e0e61377f21ad61110f
Serenity <coreyzalewski@gmail.com>
2016-09-01 14:41:20 -0400

    Adding template

---
+++ Lab02/Robot.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// API?
+void API();
+void API()
+{
+	
+}
+
+// Create World
+void createWorld();
+void createWorld()
+{
+
+}
+// Rand Positions
+
+// Run4Gold
+	// Check for available gold
+	// Check next Square
+	// Check for bomb
+	// jump next Square
+	// Check Gold
+		// Get Gold
+
+int main()
+{
+
+	return 0;
+}
