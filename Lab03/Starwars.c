f79a6730bc1010093f3bbc46accda7888fe4f18d
xzsaw <sawxuezheng01@gmail.com>
2016-09-08 15:14:55 -0400

    Added memory allocation for Clones depending on user input

--- Lab03/Starwars.c
+++ Lab03/Starwars.c
@@ -1,4 +1,4 @@
-#Author: Saw Xue Zheng, Corey Zalewski
+//Author: Saw Xue Zheng, Corey Zalewski
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +12,7 @@ struct data_clone{
 };
 
 // Function Header
-void CreateClone();
+int CreateClone(struct data_clone* warrior);
 void Input();
 void ClockDec(struct data_clone clone);
 bool LifeCheck(struct data_clone clone);
@@ -21,8 +21,20 @@ void LifeSpan();
 void SWAPI(void);
 
 // Function Implementation
-void CreateClone(){
 
+
+//Description: Ask user to create a number of clones and input the details of the clones
+int CreateClone(struct data_clone* warrior){
+
+  int n_clone = 0;
+
+  printf("Please input the number of clones to spawn: "); 
+  scanf("%d", &n_clone);
+
+  printf("Spawning %d clones...\n", n_clone);
+  warrior = (struct data_clone*) malloc(n_clone*sizeof(struct data_clone));
+
+  return n_clone;
 }
 
 void Input(){
@@ -48,9 +60,28 @@ void LifeSpan(){
 // API
 void SWAPI(void){
 
+
+  //Pointer to clones
+  struct data_clone* warrior;
+
+  //Create some clones
+  int n_clone = CreateClone(warrior);
+
+  //Begin lifespan countdown
+  LifeSpan();
+
+
+  //Deallocate when exiting
+  free(warrior);
+
 }
 
 int main(){
   // API Call
+  
+  struct data_clone* warrior;
+  int n_clone = CreateClone(warrior);
+  
+  free(warrior);
   return 0;
 }
