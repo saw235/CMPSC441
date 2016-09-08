6051e0ebad17e574d927288bb6f54bde11c7f6dc
Serenity62 <coreyzalewski@gmail.com>
2016-09-08 15:40:04 -0400

    Added some more functionality to LifeSpan and Print

--- Lab03/Starwars.c
+++ Lab03/Starwars.c
@@ -37,12 +37,41 @@ bool LifeCheck(struct data_clone *clone){  // -- Corey
   return clone->timeCounter > 0;
 }
 
-void Print(struct data_clone *clone){  // -- Corey
-  
+void Print(struct data_clone *clone, int alive){  // -- Corey
+  if(alive == 0){
+    printf("All clones are dead");
+  }
+  else{
+    printf("ID: %i\nName: %s\nTime Counter: %i\n", clone->ID, clone->name, clone->timeCounter);
+    if(LifeCheck(clone)){
+      printf("Clone is alive\n");
+    }
+    else{
+      printf("Clone is DEAD!!!\n");
+    }
+  }
 }
 
 void LifeSpan(struct data_clone *clone, int n_clone){ //-- Corey
-  int clock = 0, dead = 0;
+  int clock = 0, alive = n_clone;
+  do{
+    printf("Time: %i\n", clock);
+    if(alive != 0){	// Make sure all the clones are not dead
+      for(int i = 0; i < n_clone; i++){
+        if(!LifeCheck(clone[i]){	// Check if they are dead
+          alive--;	// decrement the number of alive clones
+        }
+        else{
+          ClockDec(clone[i]);	// decrement life clock
+          if(!LifeCheck(clone[i]){ // if they died now
+            alive--;  // decrement again
+          }
+        }
+        Print(clone[i], alive);	// Print out the clone
+      }
+    }
+    clock++;	// Increment the time clock
+  }while(alive != 0);
 }
 
 // API
