6f0ce089f5b8492164546c67169a6d39c5d57e96
Serenity62 <coreyzalewski@gmail.com>
2016-09-15 14:44:29 -0400

    Oops

---
+++ Lab04/Satellite.cpp
@@ -0,0 +1,35 @@
+//Author: Saw Xue Zheng, Corey Zalewski
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+// Defines
+
+// Global
+
+// Structs
+struct data_country{
+  char name[15];
+  int activated;
+  int canTake;
+  int selectedPack;
+};
+
+struct data_queue{
+  int country;
+  int waiting;
+};
+
+// Function Headers
+
+// Function Declaration
+
+// API Declaration
+
+int main(){
+  // API Call
+  return 0;
+}
