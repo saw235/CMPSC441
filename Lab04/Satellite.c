fca16348bbb8844051801638c15a8d8d97546711
Serenity62 <coreyzalewski@gmail.com>
2016-09-15 14:20:52 -0400

    Template for Lab 4

---
+++ Lab04/Satellite.c
@@ -0,0 +1,24 @@
+//Author: Saw Xue Zheng, Corey Zalewski
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+// Defines
+
+// Global
+
+// Structs
+
+// Function Headers
+
+// Function Declaration
+
+// API Declaration
+
+int main(){
+  // API Call
+  return 0;
+}
